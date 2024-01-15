eb67838cb8f0675b06b6feec0646e7c7ee8716d3
Fabian Deutsch <fabiand@fedoraproject.org>
2024-01-15 15:48:04 +0100

    Cleanup
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- contrib/agent.sh
+++ contrib/agent.sh
@@ -9,58 +9,61 @@ STRATEGY=${STRATEGY:-ortho}
 SWAPPINESS=${SWAPPINESS:-60}
 SWAP_SIZE_MB=${SWAP_SIZE_MB:-4000}  # RISKY dos node? was 100
 
+
+_set() { echo "Setting $1 > $2" ; echo "$1" > "$2" ; }
+
 addSwapToThisNode() {
-	set -x
-	# For debug
-	grep wasp.file /proc/swaps && swapoff -v $FSROOT/var/tmp/wasp.file
+  set -x
+  # For debug
+  grep wasp.file /proc/swaps && swapoff -v $FSROOT/var/tmp/wasp.file
 
-	grep wasp.file /proc/swaps || {
-		local SWAPFILE=$FSROOT/var/tmp/wasp.file
-		dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_SIZE_MB
-		#fallocate -z -l ${SWAP_SIZE_MB}M $SWAPFILE
-		chmod 0600 $SWAPFILE
-		mkswap $SWAPFILE
-		swapon $SWAPFILE
-	}
+  grep wasp.file /proc/swaps || {
+    local SWAPFILE=$FSROOT/var/tmp/wasp.file
+    dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_SIZE_MB
+    #fallocate -z -l ${SWAP_SIZE_MB}M $SWAPFILE
+    chmod 0600 $SWAPFILE
+    mkswap $SWAPFILE
+    swapon $SWAPFILE
+  }
 
-	[[ -n "$SWAPPINESS" ]] && { echo "$SWAPPINESS" > "$FSROOT/proc/sys/vm/swappiness"; }
-
-	local SWAP_KBYTES=$(grep wasp.file /proc/swaps | awk '{print $3;}')
-	local SWAP_MBYTES=$(( $SWAP_KBYTES / 1024 ))
+  [[ -n "$SWAPPINESS" ]] && { _set "$SWAPPINESS" "$FSROOT/proc/sys/vm/swappiness"; }
 }
 
+tune_system_slice() {
+  echo "Tuning system.slice"
+
+  # Disable swap for system.slice
+  _set 0 $FSROOT/sys/fs/cgroup/system.slice/memory.swap.max
+
+  # Set latency target to protect the root slice from io trash
+  MAJMIN=$(findmnt $FSROOT/ --output MAJ:MIN -n | sed "s/:.*/:0/")  # fixme can be manually provided
+  echo "Using MAJMIN $MAJMIN"
+  _set "$MAJMIN target=50" $FSROOT/sys/fs/cgroup/system.slice/io.latency
+}
 
 install_oci_hook() {
-	# FIXME we shoud set noswap for all cgroups, not just leaves, just to be sure
-	echo "installing hook"
+  # FIXME we shoud set noswap for all cgroups, not just leaves, just to be sure
+  echo "installing hook"
 
-	# Disable swap for system.slice
-	_set() { echo "Setting $1 > $2" ; echo "$1" > "$2" ; }
-	_set 0 $FSROOT/sys/fs/cgroup/system.slice/memory.swap.max
-
-        # Set latency target to protect the root slice from io trash
-        MAJMIN=$(findmnt $FSROOT/ --output MAJ:MIN -n | sed "s/:.*/:0/")  # fixme can be manually provided
-	echo "Using MAJMIN $MAJMIN"
-        _set "$MAJMIN target=50" $FSROOT/sys/fs/cgroup/system.slice/io.latency
-
-	cp hook.sh $FSROOT/opt/oci-hook-swap.sh
-	cp hook.json $FSROOT/run/containers/oci/hooks.d/swap-for-burstable.json
+  cp hook.sh $FSROOT/opt/oci-hook-swap.sh
+  cp hook.json $FSROOT/run/containers/oci/hooks.d/swap-for-burstable.json
 }
 
 main() {
-	# FIXME hardlinks are broken if FSROOT is used, but we need it
-	[[ ! -d /run/containers ]] && ln -s $FSROOT/run/containers /run/containers
+  # FIXME hardlinks are broken if FSROOT is used, but we need it
+  [[ ! -d /run/containers ]] && ln -s $FSROOT/run/containers /run/containers
 
-	addSwapToThisNode
-	install_oci_hook
+  addSwapToThisNode
+  tune_system_slice
+  install_oci_hook
 
-	echo "Done"
+  echo "Done"
 
-	sleep inf
+  sleep inf
 }
 
 swaptop() {
-	while sleep 0.5 ; do D=$(uptime ; free -m ; find /sys/fs/cgroup -name memory.swap.current | while read FN ; do [[ -f "$FN" && "$(cat $FN)" -gt 0 ]] && { echo -n "$FN " ; numfmt --to=iec-i --suffix=B < $FN ; } ; done | sort -r -k 2 -h) ; clear ; echo "$D" | head -n 30 ; done
+  while sleep 0.5 ; do D=$(uptime ; free -m ; find /sys/fs/cgroup -name memory.swap.current | while read FN ; do [[ -f "$FN" && "$(cat $FN)" -gt 0 ]] && { echo -n "$FN " ; numfmt --to=iec-i --suffix=B < $FN ; } ; done | sort -r -k 2 -h) ; clear ; echo "$D" | head -n 30 ; done
 }
 
 ${@:-main}
--- to.sh
+++ to.sh
@@ -1,35 +1,39 @@
 IMG_REPO=quay.io/fdeutsch/wasp-operator-prototype
 
 build() {
-	podman -r build -t $IMG_REPO -f Dockerfile .
+  podman -r build -t $IMG_REPO -f Containerfile .
 }
 
 push() {
-	podman -r push $IMG_REPO
+  podman -r push $IMG_REPO
 }
 
 _oc() { echo "$ oc $@" ; oc $@ ; }
 qoc() { oc $@ > /dev/null 2>&1; }
 
 apply() {
-	_oc apply -f manifests/ds.yaml
+  _oc apply -f manifests/ds.yaml
+}
+
+patch() {
+  _oc patch --type=merge  -f manifests/prep-hco.yaml --patch-file manifests/prep-hco.yaml || :
 }
 
 deploy() {
-	local NS=wasp
-	qoc get project wasp || _oc adm new-project $NS
-	_oc project $NS
-	qoc get sa -n $NS wasp || {
-		_oc create sa -n $NS wasp
-		#oc adm policy add-role-to-user -n $NS cluster-admin -z wasp
-		_oc adm policy add-cluster-role-to-user cluster-admin -z wasp
-		_oc adm policy add-scc-to-user -n $NS privileged -z wasp
-	}
-	apply
+  local NS=wasp
+  local SA=$NS
+  qoc get project $NS || _oc adm new-project $NS
+  _oc project $NS
+  qoc get sa -n $NS $SA || {
+    _oc create sa -n $NS $SA
+    _oc adm policy add-cluster-role-to-user cluster-admin -z $SA
+    _oc adm policy add-scc-to-user -n $NS privileged -z $SA
+  }
+  apply
 }
 
 destroy() {
-	_oc delete -f manifests/ds.yaml
+  _oc delete -f manifests/ds.yaml
 }
 
 eval "$@"
