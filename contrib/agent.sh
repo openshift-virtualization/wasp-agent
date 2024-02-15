66eb4c46b9128c2b6059c88dcd937a199db9ee62
Fabian Deutsch <fabiand@fedoraproject.org>
2024-02-15 12:50:23 +0100

    agent: Configure softEviction
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- contrib/agent.sh
+++ contrib/agent.sh
@@ -19,9 +19,20 @@ tune_system_slice() {
   _set "$MAJMIN target=50" $FSROOT/sys/fs/cgroup/system.slice/io.latency
 
   echo "Tune kubepods.slice"
-  MEM_HIGH_PERCENT=5
-  MEM_HIGH=$(( $(< $FSROOT/sys/fs/cgroup/kubepods.slice/memory.max) - $(< $FSROOT/sys/fs/cgroup/kubepods.slice/memory.max) / $MEM_HIGH_PERCENT ))
-  _set $MEM_HIGH $FSROOT/sys/fs/cgroup/kubepods.slice/memory.high
+  # Gi is pow2
+  {
+    Ki=1024  Mi=$((Ki * Ki))  Gi=$((Mi * Ki))  Ti=$((Gi * Ki))
+    KB=1000 MB=$((KB * KB)) GB=$((MB * KB)) TB=$((GB * KB))
+
+    MEM_MAX=$(< $FSROOT/sys/fs/cgroup/kubepods.slice/memory.max)
+
+    # We need to get this from kubelet.conf
+    THRESHOLD_BYTES=$(( 1*Gi ))
+
+    MEM_HIGH=$(( MEM_MAX - THRESHOLD_BYTES ))
+
+    _set $MEM_HIGH $FSROOT/sys/fs/cgroup/kubepods.slice/memory.high
+  }
 }
 
 install_oci_hook() {
