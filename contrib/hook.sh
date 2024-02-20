7ce0268ca685d68ceeb57fd0cbe5b3905e3bce51
Fabian Deutsch <fabiand@fedoraproject.org>
2024-02-20 09:18:24 +0100

    hook.sh: Don't spawn grep, optimize with bash
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- contrib/hook.sh
+++ contrib/hook.sh
@@ -3,10 +3,8 @@
 echo "WASP SWAP hook"
 
 set -x
-date
-env
 
 CG_PATH=$(cat config.json | jq -r '.linux.cgroupsPath')
 CONTAINERID=$(echo "$CG_PATH" | cut -d: -f3)
 
-grep burst <<<$CG_PATH && echo max > /sys/fs/cgroup/kubepods.slice/*burst*/*/crio-$CONTAINERID*/memory.swap.max || :
+[[ "$CG_PATH" = *burst* ]] && echo max > /sys/fs/cgroup/kubepods.slice/*burst*/*/crio-$CONTAINERID*/memory.swap.max || :
